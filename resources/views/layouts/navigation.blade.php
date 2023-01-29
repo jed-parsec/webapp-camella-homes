897ea216c2332149f40cf68f504c94430b1347ab
jed-parsec <edulanjomari32@gmail.com>
2023-01-30 02:42:04 +0800

    fixed dashboard texts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Announcements') }}
                     </x-nav-link>
                     @if(Auth::user()->hasRole('admin'))
                         <x-nav-link :href="route('admin.list-user')" :active="request()->routeIs('admin.list-user')">
